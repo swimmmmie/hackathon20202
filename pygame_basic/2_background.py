d198885fb6e6ed4b70545bf94fff91ef347faa8b
swimmmmie <dongaseu@naver.com>
2020-12-20 05:11:34 +0900

    20202

---
+++ pygame_basic/2_background.py
@@ -0,0 +1,23 @@
+import pygame
+
+pygame.init()
+
+screen_width = 480
+screen_height = 640
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+pygame.display.set_caption("Nado Game")
+
+background = pygame.image.load("C:/Users/김수영/Desktop/pythonProject/pygame_basic/background.png")
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.blit(background, (0, 0))
+
+    pygame.display.update()
+
+pygame.quit()
